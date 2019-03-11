c2a5fce8c65eebc44b48640e154e0206c90e0b85
Samuel <coolgen4@outlook.com>
2019-03-11 07:35:48 +0100

    new ui

--- home.js
+++ home.js
@@ -1,21 +1,21 @@
-function checkform(){
-	var fn= document.getElementById('fname');
-	var ln= document.getElementById('lname');
-	var em= document.getElementById('em');
-	var ps=document.getElementById('pass');
-	 if ( fn.value =="" | ln.value == "" || em.value == "" || ps.value == ""){
-	 	document.write("You must fill in the missing fields");
-	 } else {
-	 	location.href="signin.html";
-	 }
-}
-function checkSign(){
-	var em=document.getElementById('txtemail');
-	var ps=document.getElementById('txtpass');
-	if ( fn.value =="" | ln.value == "" || em.value == "" || ps.value == ""){
-
-	} else{
-		location.href="dashboard.html";
+function checkform() {
+	let fn = document.getElementById('fname');
+	let ln = document.getElementById('lname');
+	let em = document.getElementById('em');
+	let  ps = document.getElementById('pass');
+	if (fn.value == "" | ln.value == "" || em.value == "" || ps.value == "") {
+		document.write("You must fill in the missing fields");
+	} else {
+		location.href = "signin.html";
 	}
-	
+}
+function checkSign() {
+	let em = document.getElementById('txtemail');
+	let ps = document.getElementById('txtpass');
+	if (fn.value == "" | ln.value == "" || em.value == "" || ps.value == "") {
+
+	} else {
+		location.href = "dashboard.html";
+	}
+
 }
